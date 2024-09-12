0d5ab951cceadc312070c990e4bd5d4aeb760bd4
Aiden Pike <aidenpike113@gmail.com>
2024-09-12 18:51:19 -0400

    Check off flagship work

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main(){
     /*
         What will I need?
-            Flagship logic (header and class, include power cells)
+            Flagship logic (header and class, include power cells) [DONE]
             Equipment card stuff (inFile probably, equipment card header and class)
             Map work (header and class)
             Game loop
