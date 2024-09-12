410bb3b70e3157f1a21d5fc4d21f3f34e325528e
Aiden Pike <aidenpike113@gmail.com>
2024-09-12 18:37:47 -0400

    Define getters

--- src/flagship.cpp
+++ src/flagship.cpp
@@ -1,5 +1,29 @@
 #include "..\include\flagship.hpp"
 
 flagship::flagship(int hull, int shield, int attack, int speed, int power_cells){
-    
+    m_hull = hull;
+    m_shield = shield;
+    m_attack = attack;
+    m_speed = speed;
+    m_power_cells = power_cells;
+}
+
+int flagship::get_hull(){
+    return m_hull;
+}
+
+int flagship::get_shield(){
+    return m_shield;
+}
+
+int flagship::get_attack(){
+    return m_attack;
+}
+
+int flagship::get_speed(){
+    return m_speed;
+}
+
+int flagship::get_cell_count(){
+    return m_power_cells;
 }
